404124a90d0df8e88860bafbd6f4599ded0ecab8
Paul <paulsolal@yahoo.fr>
2025-07-30 16:36:38 +0200

    Added working Payment Flow on Android side

--- Sources/SkipProject/Skip/Payments.kt
+++ Sources/SkipProject/Skip/Payments.kt
@@ -1,8 +1,12 @@
 package skip.project
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
 
 @Composable
 fun StripeCheckoutView(
@@ -11,10 +15,42 @@ fun StripeCheckoutView(
     paymentIntentSecret: String,
     publishableKey: String
 ) {
-    Column {
-        Text("Customer ID: $customerId")
-        Text("Ephemeral Key: $ephemeralKey")
-        Text("PaymentIntent Secret: $paymentIntentSecret")
-        Text("Publishable Key: $publishableKey")
+    val paymentSheet = remember { PaymentSheet.Builder(::onPaymentSheetResult) }.build()
+    val context = LocalContext.current
+
+    var customerConfig by remember { mutableStateOf<PaymentSheet.CustomerConfiguration?>(null) }
+    var clientSecret by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(customerId, ephemeralKey, paymentIntentSecret, publishableKey) {
+        PaymentConfiguration.init(context, publishableKey)
+        clientSecret = paymentIntentSecret
+        customerConfig = PaymentSheet.CustomerConfiguration(
+            id = customerId,
+            ephemeralKeySecret = ephemeralKey
+        )
+    }
+
+    Button(onClick = {
+        val config = customerConfig
+        val secret = clientSecret
+        if (config != null && secret != null) {
+            paymentSheet.presentWithPaymentIntent(
+                secret,
+                PaymentSheet.Configuration.Builder("My merchant name")
+                    .customer(config)
+                    .allowsDelayedPaymentMethods(true)
+                    .build()
+            )
+        }
+    }) {
+        Text("Checkout")
+    }
+}
+
+private fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
+    when (paymentSheetResult) {
+        is PaymentSheetResult.Canceled -> println("Canceled")
+        is PaymentSheetResult.Failed -> println("Error: ${paymentSheetResult.error}")
+        is PaymentSheetResult.Completed -> println("Completed")
     }
 }
